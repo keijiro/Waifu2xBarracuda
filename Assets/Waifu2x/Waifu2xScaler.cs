6a8d48617620bb45b4e9622a6ea6242152badea8
Keijiro Takahashi <keijiro@gmail.com>
2021-01-10 22:36:19 +0900

    Remove Util.cs

--- Assets/Waifu2x/Waifu2xScaler.cs
+++ Assets/Waifu2x/Waifu2xScaler.cs
@@ -80,4 +80,16 @@ sealed public class Waifu2xScaler : System.IDisposable
     #endregion
 }
 
+static class ComputeShaderExtensions
+{
+    static int[] i2 = new int[2];
+
+    public static void SetInts
+      (this ComputeShader cs, string name, int x, int y)
+    {
+        i2[0] = x; i2[1] = y;
+        cs.SetInts(name, i2);
+    }
+}
+
 } // namespace Waifu2x
